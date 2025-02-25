7169bfb117cdbaf461502a48b920e9135b3d14ee
Vryllon <noahvader@yahoo.com>
2025-02-25 11:35:53 -0500

    Implimented getImage function

--- EdgeDetection.cpp
+++ EdgeDetection.cpp
@@ -4,29 +4,49 @@
 // *** NOTE *** some function types may need to change from skeleton to implimentation
 
 #include <vector>
+#include <iostream>
+#include <fstream>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 using namespace std;
 
-// Main function to run everything
-int main()
+// Vector2 struct
+struct Vector2 {
+    double x, y;
+    
+    Vector2( double x = 0, double y = 0 ) : x(x), y(y) {}
+};
+
+// Gets image from file path and returns gray image as 2d vector
+Vector2 * getImage(const char* imgPath) 
 {
+    int width, height, channels;
+    unsigned char *img = stbi_load(imgPath, &width, &height, &channels, 0);
+    if(img == NULL || channels != 3) 
+    {
+        printf("Error in loading the image\n");
+        exit(1);
+    }
+    printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
-    // run functions step by step in sequential order
+    size_t img_size = width * height * channels;
+    size_t gray_img_size = width * height;
+    
+    Vector2 *gray_img = (Vector2 *)malloc(gray_img_size * sizeof(Vector2));
+    if(gray_img == NULL) {
+        printf("Unable to allocate memory for the gray image.\n");
+        exit(1);
+    }
 
-    return 0;
-}
+    int i = 0;
+    for(unsigned char *p = img; p != img + img_size; p += channels, i += 1) 
+    {
+        gray_img[i] = Vector2(((*p + *(p + 1) + *(p + 2))/3.0), 0.0);
+    }
 
-
-// Getting image from somewhere (Initial implimentation will likely skip this step and set a predefined 2d array later)
-void getImage() // should return image and not void
-{
-
-}
-
-
-
-// Reduce image to a grayscale
-void toGrayScale() // should return grayscaled image and should have an image parameter
-{
+    return gray_img;
 
 }
 
@@ -38,15 +58,7 @@ void gausianFilter() // should return filtered image and have grayscaled image a
     
 }
 
-
-
 // Create a 2d array (vectors) of vector2's (structure) that represent the intesity gradients in the image (direction of edges)
-struct Vector2 {
-    int x, y;
-    
-    Vector2( int x = 0, int y = 0 ) : x(x), y(y) {}
-};
-
 vector<vector<Vector2>> intensityGradient( int width, int height ) // parameters are temporary unless this works well (probably change to smoothed image as parameter)
 {
     // Create a 2D vector of Vector2 objects to store intensity gradients.
@@ -89,4 +101,18 @@ void convertToImage( vector<vector<Vector2>> intensityGradients ) // return type
 
     // convert intesity gradients to an image format
 
+}
+
+// Main function to run everything
+int main()
+{
+
+    // run functions step by step in sequential order
+    
+    Vector2 *img = getImage("./images/NYScene.jpg");
+
+    // free img allocation
+    stbi_image_free(img);
+
+    return 0;
 }
