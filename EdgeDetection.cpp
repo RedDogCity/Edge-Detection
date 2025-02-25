089a3533c2e6c9b7c5c72a31b96dcd0d5e7ca2f8
Vryllon <noahvader@yahoo.com>
2025-02-25 11:56:26 -0500

    adjusted the return types and parameters of each function

--- EdgeDetection.cpp
+++ EdgeDetection.cpp
@@ -3,7 +3,6 @@
 
 // *** NOTE *** some function types may need to change from skeleton to implimentation
 
-#include <vector>
 #include <iostream>
 #include <fstream>
 #define STB_IMAGE_IMPLEMENTATION
@@ -19,61 +18,77 @@ struct Vector2 {
     Vector2( double x = 0, double y = 0 ) : x(x), y(y) {}
 };
 
+// Intensity gradient struct
+struct IntensityGradientImage {
+    int height, width;
+    Vector2 * intensityGradientArray; 
+    
+    IntensityGradientImage( int h, int w, Vector2 * iga ) : height(h), width(w), intensityGradientArray(iga) {}
+};
+
 // Gets image from file path and returns gray image as 2d vector
-Vector2 * getImage(const char* imgPath) 
+IntensityGradientImage getImage(const char* imgPath) 
 {
+    // load the image from the file path provided
     int width, height, channels;
     unsigned char *img = stbi_load(imgPath, &width, &height, &channels, 0);
-    if(img == NULL || channels != 3) 
+    if(img == NULL || channels != 3) // exit if the image is not found or is incorrect number of channels
     {
         printf("Error in loading the image\n");
         exit(1);
     }
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
+    // initialize image size variables
     size_t img_size = width * height * channels;
     size_t gray_img_size = width * height;
     
+    // allocate space for the intensity gradient array
     Vector2 *gray_img = (Vector2 *)malloc(gray_img_size * sizeof(Vector2));
     if(gray_img == NULL) {
-        printf("Unable to allocate memory for the gray image.\n");
+        printf("Unable to allocate memory for the gray image intensity gradient array.\n");
         exit(1);
     }
 
+    // Convert each colored pixel in the original image to an average of the rgb values
+    // Store these pixel values in the intensities and initialize the gradient values to 0.0
     int i = 0;
     for(unsigned char *p = img; p != img + img_size; p += channels, i += 1) 
     {
         gray_img[i] = Vector2(((*p + *(p + 1) + *(p + 2))/3.0), 0.0);
     }
 
-    return gray_img;
+    // create the actual Intensity Gradient Image variable using the creted array and the image dimensions
+    IntensityGradientImage ret(height, width, gray_img);
+
+    return ret;
 
 }
 
 
 
 // Apply a gausian filter to the image to smooth and remove noise
-void gausianFilter() // should return filtered image and have grayscaled image as parameter
+IntensityGradientImage gausianFilter(IntensityGradientImage img)
 {
     
+
+    return img;
 }
 
 // Create a 2d array (vectors) of vector2's (structure) that represent the intesity gradients in the image (direction of edges)
-vector<vector<Vector2>> intensityGradient( int width, int height ) // parameters are temporary unless this works well (probably change to smoothed image as parameter)
+IntensityGradientImage intensityGradient(IntensityGradientImage intensityGradients)
 {
-    // Create a 2D vector of Vector2 objects to store intensity gradients.
-    vector<vector<Vector2>> gradients(height, vector<Vector2>(width));
 
     // Calculate the intensity gradient of each pixel (round direction to the be one of the four edge directions ( | , - , / , \ ) )
     
 
-    return gradients;
+    return intensityGradients;
 }
 
 
 
 // Apply gradient magnitude thresholding to find actual image edges from the gradients
-vector<vector<Vector2>> magnitudeThreshold( vector<vector<Vector2>> intensityGradients , double weakThresh ) // parameters are temporary unless this works well (probably change to smoothed image as parameter)
+IntensityGradientImage magnitudeThreshold( IntensityGradientImage intensityGradients , double weakThresh )
 {
     
     // Remove all edges below threshold (set to 0 maybe)
@@ -85,7 +100,7 @@ vector<vector<Vector2>> magnitudeThreshold( vector<vector<Vector2>> intensityGra
 
 // Filter out noisey edges using threshold filter 
 // (Strong edges are accepted, weak are accepted if connected to strong)
-vector<vector<Vector2>> hysteresis( vector<vector<Vector2>> intensityGradients , double strongThresh ) // parameters are temporary unless this works well (probably change to smoothed image as parameter)
+IntensityGradientImage hysteresis( IntensityGradientImage intensityGradients , double strongThresh )
 {
     
     // Remove all edges below threshold that arent connected to strong edge (set to 0 maybe)
@@ -95,11 +110,11 @@ vector<vector<Vector2>> hysteresis( vector<vector<Vector2>> intensityGradients ,
 
 
 
-// Convert intesity gradients to image to show edges image (or midstep images)
-void convertToImage( vector<vector<Vector2>> intensityGradients ) // return type should be an image
+// Displays (sends to an output folder) image given by intensity gradient array
+void displayImage( Vector2 * intensityGradients )
 {
 
-    // convert intesity gradients to an image format
+
 
 }
 
@@ -107,12 +122,14 @@ void convertToImage( vector<vector<Vector2>> intensityGradients ) // return type
 int main()
 {
 
-    // run functions step by step in sequential order
     
-    Vector2 *img = getImage("./images/NYScene.jpg");
+    // Get the image file an prep for transformation
+    IntensityGradientImage img = getImage("./images/NYScene.jpg");
+
+
 
     // free img allocation
-    stbi_image_free(img);
+    stbi_image_free(img.intensityGradientArray);
 
     return 0;
 }
